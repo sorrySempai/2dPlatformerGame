7c96710d38bb3b8ccb10613d41e7a15226091ecc
Dmitriy Masterov <masterovdmitriyvit@gmail.com>
2021-05-12 16:38:26 +1000

    v0.9.3
    -сделано сохранение уровней - теперь с самого начала доступен только первый, а открываются они по мере прохождения

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public static LevelController instance = null;
+    int sceneIndex;
+    int levelComplete;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } 
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        levelComplete = PlayerPrefs.GetInt("LevelComplete");
+
+    }
+
+
+    public void IsEndGame()
+    {
+        if (sceneIndex == 5)
+        {
+            Invoke("EndScene", 1f);
+        }
+        else
+        {
+            if (levelComplete < sceneIndex)
+                PlayerPrefs.SetInt("LevelComplete", sceneIndex);
+            Invoke("NextLevel", 1f);
+        }
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+
+    void EndScene()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private string sceneName;
-    public void PlayGame()
+    int levelComplete;
+
+    public Button lvl2;
+    public Button lvl3;
+    public Button lvl4;
+    public Button lvl5;
+
+
+    public void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        levelComplete = PlayerPrefs.GetInt("LevelComplete");
+        lvl2.interactable = false;
+        lvl3.interactable = false;
+        lvl4.interactable = false;
+        lvl5.interactable = false;
+
+        switch (levelComplete)
+        {
+            case 1:
+                lvl2.interactable = true;
+                break;
+            case 2:
+                lvl2.interactable = true;
+                lvl3.interactable = true;
+                break;
+            case 3:
+                lvl2.interactable = true;
+                lvl3.interactable = true;
+                lvl4.interactable = true;
+                break;
+            case 4:
+                lvl2.interactable = true;
+                lvl3.interactable = true;
+                lvl4.interactable = true;
+                lvl5.interactable = true;
+                break;
+        }
+    }
+
+    public void LoadTo(int lvl)
+    {
+        SceneManager.LoadScene(lvl);
     }
 }
